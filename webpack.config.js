a2427adfac062c42912ee32277c0ecbd21e1a6f5
SergeyKagal <3461995@gmail.com>
2022-06-07 01:16:49 +0300

    init: add invironment

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'index.html'
+            // favicon: './assets/images/favicon.ico'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    devServer: {
+        port: 3000
+    }
+};
