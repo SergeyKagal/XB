17a7d7f4ce0104245a02fa2c76ebdc0e42ad3b8d
SergeyKagal <3461995@gmail.com>
2022-06-07 12:39:57 +0300

    feat: delete tags

---
+++ src/app/delete-handler.js
@@ -0,0 +1,27 @@
+import { tags } from '..';
+import { drawTags } from './draw-tags';
+import { saveTags } from './save-tags';
+
+export const deleteListenerOn = () => {
+  const list = document.querySelector('.tag__list');
+  list.addEventListener('click', deleteHandler);
+};
+export const deleteListenerOff = () => {
+  const list = document.querySelector('.tag__list');
+  list.removeEventListener('clic', deleteHandler);
+};
+
+export const deleteHandler = (e) => {
+  if (e.target.className === 'tag__list-delete-button') {
+    const id = e.target.getAttribute('id');
+
+    deleteListenerOff();
+    tags.forEach((element, index) => {
+      if (element.tagId === id) {
+        tags.splice(index, 1);
+      }
+    });
+    saveTags();
+    drawTags(tags);
+  }
+};
--- src/app/draw-tags.js
+++ src/app/draw-tags.js
@@ -1,9 +1,13 @@
+import {  deleteListenerOn } from './delete-handler';
+
 export const drawTags = (tags) => {
   const list = document.querySelector('.tag__list');
+  list.innerHTML='';
   tags.forEach((element) => {
-    list.innerHTML += `<li class="tag__list-item" data-id="${element.tagId}">
+    list.innerHTML += `<li class="tag__list-item" id="${element.tagId}">
     <span class="tag__list-content">${element.content}</span>
-    <button class="tag__list-delete-button">&times;</button>
+    <button class="tag__list-delete-button" id="${element.tagId}">&times;</button>
   </li>`;
   });
+  deleteListenerOn();
 };
